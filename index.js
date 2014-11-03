6d76929bfec38ef8e116fdbedaac7a5a79904d1d
Mikko Haapoja <me@mikkoh.com>
2014-11-03 14:45:56 -0500

    You've been had by the snake

---
+++ index.js
@@ -0,0 +1,68 @@
+module.exports = lookin;
+
+var find = require( 'dom-select' ),
+	css = require( 'dom-style' ),
+	item3d = require( './lib/item3d' );
+
+var countItems = 0;
+
+function lookin( settings ) {
+
+	if( !( this instanceof lookin ) )
+		return new lookin( settings );
+
+	var s = this.s = settings || {};
+
+	this.items = {};
+
+	s.el = s.el || 'body';
+	s.perspective = s.perspective || 1000;
+
+	if( typeof s.el == 'string' )
+		this.container = find( s.el );
+	else
+		this.container = s.el;
+
+	if( !s.el )
+		throw new Error( 'el must be defined' );
+
+	css( this.container, {
+
+		position: 'relative',
+		width: '100vw',
+		height: '100vh',
+		perspective: s.perspective
+	});
+}
+
+lookin.prototype = {
+
+	create: function( settings ) {
+
+		var s = this.s,
+			name = settings.name;
+
+		settings.container = this.container;
+		settings.perspective = s.perspective;
+
+		if( name )
+			this.items[ name ] = item3d( settings );
+		else
+			item3d( settings );
+
+		return this;
+	},
+
+	get: function( name ) {
+
+		return this.items[ name ];
+	},
+
+	rotate: function( x, y ) {
+
+		css( this.container, {
+
+			'perspective-origin': ( x / 90 * 100 ) + '% ' + ( y / 90 * 100 ) + '% '			
+		});
+	}
+};
---
+++ test/index.js
@@ -0,0 +1,76 @@
+var lookin = require( './..' ),
+	css = require( 'dom-style' ),
+	HEIGHT = 500;
+
+var lWall = document.createElement( 'div' ),
+	rWall = document.createElement( 'div' ),
+	floor = document.createElement( 'div' );
+
+document.body.appendChild( floor );
+document.body.appendChild( lWall );
+document.body.appendChild( rWall );
+
+floor.id = 'floor';
+lWall.id = 'lWall';
+rWall.id = 'rWall';
+
+css( floor, {
+
+	background: '#EEE',
+	position: 'absolute',
+	left: '0%',
+	top: '50%',
+	'margin-top': ( HEIGHT * -0.5 ) + 'px',
+	width: '100%',
+	height: HEIGHT + 'px'
+});
+
+css( lWall, {
+
+	background: '#333',
+	position: 'absolute',
+	left: '0%',
+	top: '50%',
+	'margin-top': ( HEIGHT * -0.5 ) + 'px',
+	width: '200px',
+	height: HEIGHT + 'px'
+});
+
+css( rWall, {
+
+	background: '#333',
+	position: 'absolute',
+	left: '100%',
+	top: '50%',
+	'margin-top': ( HEIGHT * -0.5 ) + 'px',
+	width: '200px',
+	height: HEIGHT + 'px'
+});
+
+
+
+var scene = lookin()
+	.create( {
+
+		name: 'lWall',
+		el: '#lWall',
+	})
+	.create( {
+
+		name: 'floor',
+		el: '#floor',
+		z: -200
+	})
+	.create( {
+
+		name: 'rWall',
+		el: '#rWall'
+	});
+
+scene.get( 'lWall' ).rotateY( 90 ).apply();
+scene.get( 'rWall' ).rotateY( 90 ).apply();
+
+window.onmousemove = function( ev ) {
+
+	scene.rotate( ev.pageX / window.innerWidth * 90, ev.pageY / window.innerHeight * 90 );
+};
